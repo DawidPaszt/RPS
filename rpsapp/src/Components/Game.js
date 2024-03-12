8079034de4ab63eee5e411ee3c49105a459d951e
Dawid Paszt <dawidpaszt@gmail.com>
2024-03-12 19:06:03 +0100

    First Commit

---
+++ rpsapp/src/Components/Game.js
@@ -0,0 +1,14 @@
+var choices=["Rock","Paper","Scissors"];
+
+function Game(){
+
+return(
+    <>
+    {choices.map((choice)=>(
+        <>choice<br/></>
+    ))}
+    </>
+)
+}
+
+export default Game;
